eafa4d393de5193cfc40a344cce8b944b52dd686
Roberto <robertorebolledo151@gmail.com>
2023-04-17 09:14:34 +0200

    bucles y menu

--- src/calculadora/Principal.java
+++ src/calculadora/Principal.java
@@ -9,7 +9,39 @@ public class Principal {
 		int num1, num2,menu;
 		Scanner sc = new Scanner (System.in);
 		
+		do {
+			menu();
+			menu = Integer.parseInt(sc.nextLine());
+			switch(menu){
+				
+			case 1:
+				
+				break;
+			case 2:
+				
+				break;
+			case 3:
+				
+				break;
+			case 4:
+				
+				break;
+				
+				
+			
+			}
+			
+			
+			
+		}while(menu!=0);
 		
+	}
+	public static void menu() {
+		
+		System.out.println("1.-Sumar");
+		System.out.println("2.-Restar");
+		System.out.println("3.-Multiplicar");
+		System.out.println("4.-Dividir");
 		
 	}
 
