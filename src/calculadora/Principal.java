6d602fa99a0937ba65777dfa911106ae5c9d2c04
Roberto <robertorebolledo151@gmail.com>
2023-04-17 09:11:38 +0200

    variables

---
+++ src/calculadora/Principal.java
@@ -0,0 +1,16 @@
+package calculadora;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1, num2,menu;
+		Scanner sc = new Scanner (System.in);
+		
+		
+		
+	}
+
+}
